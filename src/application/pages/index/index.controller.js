883d6c5a93968c45dd272c21ba3f2b7f47fc8751
Sébastien THOMAS <74055963+SebastienThomasDEV@users.noreply.github.com>
2023-10-05 01:03:48 +0200

    form module

--- src/application/pages/index/index.controller.js
+++ src/application/pages/index/index.controller.js
@@ -1,4 +1,5 @@
 import {Controller} from "../../../core/vendor/controller/controller.ts";
+import FormManager from "../../../core/modules/form/formManager.ts";
 
 
 export class IndexController extends Controller {
@@ -12,6 +13,11 @@ export class IndexController extends Controller {
             title: "base",
         }
         await this.render(args);
+    }
+
+    onInit(params, html) {
+        const formManager = new FormManager();
+        return super.onInit(params, html);
     }
 
 
